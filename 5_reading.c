9c801dde63adda1676135256ab1aead6a5d6ab24
Pranav Sonawane <pranav77700@gmail.com>
2024-09-01 15:09:49 +0530

    Statement Reading of Variable

---
+++ 5_reading.c
@@ -0,0 +1,27 @@
+/*
+	Description :- Statement Reading of Variable
+	Author :- Pranav R Sonawane
+*/
+
+#include<stdio.h>
+int main()
+
+{
+	int no = 11;
+	// no is a variable is of type integer
+	// currently initialized with 11.
+
+	char ch = 'A';
+	// ch is variable of type character 
+	// currently initialized with A.
+
+	float f = 3.14;
+	// f is a variable is of type float
+	// currently initialized with 3.14.
+
+	double marks = 92.69;
+	// marks is a variable is of type double
+	// currently initialized with 92.69.
+
+	return 0
+}	
