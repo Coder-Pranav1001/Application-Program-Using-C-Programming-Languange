4164140aa7ae736d8daf797974161fca9a635b29
Pranav Sonawane <pranav77700@gmail.com>
2024-09-01 15:03:07 +0530

    Print Jay Ganesh on consol

---
+++ 1_hello.c
@@ -0,0 +1,26 @@
+/*
+	Description :- Print Jay Ganesh on consol
+	Author :- Pranav R Sonawane
+*/
+
+//# preprocessor directive symbol
+//include is a keyword
+//stdio standard input and output
+//.h header file
+#include<stdio.h>
+//"stdio.h" its also allowd but change location
+
+//int return type of function
+//main entry point function 
+int main()
+{
+	// print statement
+	printf("Jay Ganesh...\n");
+
+	// return statement
+	return 0;
+}
+
+// return 0  Succesfull termination
+// return 1  Abnormal termination
+// return -1  Erronious termination
