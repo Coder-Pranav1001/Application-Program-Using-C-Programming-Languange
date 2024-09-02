24096658b29fae8e01749a6d350e8d43a1e1ba0f
Pranav Sonawane <pranav77700@gmail.com>
2024-09-02 19:06:35 +0530

    function prototype and its statement reading

---
+++ 6_function2.c
@@ -0,0 +1,29 @@
+/*
+	Description :- function prototype and its statement reading
+	Author :- Pranav R Sonawane
+*/
+
+#include<stdio.h>
+
+// Demo is a function which accepts nothing 
+// and which return nothing(void).
+void Demo() // Function Parameter
+{
+	 
+}
+
+// Hello is a function which accepts two parameters
+// first paramenter is integer and second paramenter is character
+// and that functon return float
+float Hello(int i, char ch)
+{
+	
+}
+
+// main is a function which accept nothing
+// return integer .
+int main()
+{
+	
+	return 0;
+}
