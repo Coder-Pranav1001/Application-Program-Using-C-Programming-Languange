8fd24121b5be985b8b625fa6e94c5079eb519b16
Pranav Sonawane <pranav77700@gmail.com>
2024-09-02 19:11:16 +0530

    Storage class
    // Storage class
    // extern storage class
    // second file

---
+++ 10_extern2.c
@@ -0,0 +1,15 @@
+/*
+	Description :- Storage class, extern storage class
+	Author :- Pranav R Sonawane
+*/
+
+// Storage class
+// extern storage class
+// second file
+
+// by default extern variable
+// Globle variables ( naked variable )
+int no = 11; // defination of variable
+int A = 21;  
+
+// gcc extern1.c extern2.c -o exe
