c661b57d4f6b1d3bd24191d77529a63d383144bf
Pranav Sonawane <pranav77700@gmail.com>
2024-09-03 20:12:57 +0530

    Storage class
    // Storage class
    // Global static
    // second file

---
+++ 12_static2.c
@@ -0,0 +1,13 @@
+/*
+	Description :- Storage class, Global static storage class
+	Author :- Pranav R Sonawane
+*/
+
+// Storage class
+// Global static
+// second file
+
+static int no = 11; // Global static variable
+int A = 10; // naked variable
+
+// gcc static1.c static2.c -o exe  
