987d64bad78f2ca82a67f2069f85eba81cdee40e
Pranav Sonawane <pranav77700@gmail.com>
2024-09-15 19:17:51 +0530

    user defined header file
    user defined header file

---
+++ 53_marvellous.h
@@ -0,0 +1,18 @@
+/*
+	Description :- user defined header file
+	Author :- Pranav R Sonawane
+*/
+
+// Macro defination
+#define MAX 101
+
+// Function prototype
+void fun();
+
+// Structure Declaration
+struct Demo
+{
+	int iNo;
+	int iValue;
+	float fValue;
+};
