91890bfedc578ded4941dd7cdafc232e539e449e
Pranav Sonawane <pranav77700@gmail.com>
2024-09-15 19:20:00 +0530

    Header file inside another header file
    Header file inside another header file

---
+++ 55_marvellous.h
@@ -0,0 +1,22 @@
+/*
+	Description :- Header file inside another header file 
+	Author :- Pranav R Sonawane
+*/
+
+// Header file inclusion 
+#include<stdio.h>
+
+// Macro defination
+#define MAX 1000
+
+// Function prototype
+void fun();
+
+// Structure Declaration
+struct Demo
+{
+	int iNo;
+	int iValue;
+	float fValue;
+	float f;
+};
