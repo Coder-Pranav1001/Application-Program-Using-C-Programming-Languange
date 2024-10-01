3ad0563d3caca8b3e714847ee0c57522cca827ec
Pranav Sonawane <pranav77700@gmail.com>
2024-10-02 00:14:24 +0530

    print Jay Ganesh using while loop
    // while loop
    // print Jay Ganesh using while loop

---
+++ 62_while1.c
@@ -0,0 +1,21 @@
+/*
+	Description :- Loops(while loop)
+	Author :- Pranav R Sonawane
+*/
+
+// while loop
+// print Jay Ganesh using while loop
+
+#include<stdio.h>
+int main()
+{
+	int iCnt = 0; // Counter 
+	
+	while(iCnt <= 4)
+	{
+		printf("Jay Ganesh...\n");
+		iCnt++;
+	}
+	
+	return 0;
+}
