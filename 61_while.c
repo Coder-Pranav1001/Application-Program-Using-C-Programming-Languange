39c42f52600117bce7e67cd4cbbfd8ebd8134ab5
Pranav Sonawane <pranav77700@gmail.com>
2024-10-02 00:13:36 +0530

    Print Jay Ganesh.. Sequentially

---
+++ 61_while.c
@@ -0,0 +1,27 @@
+/*
+	Description :- Loops
+	Author :- Pranav R Sonawane
+*/
+
+// Print Jay Ganesh.. Sequentially
+
+#include<stdio.h>
+int main()
+{
+	// Sequance
+	
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	printf("Jay Ganesh...\n");
+	
+	return 0;
+}
