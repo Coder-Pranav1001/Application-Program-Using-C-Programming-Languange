07deef52abc0c16ccc2264f1d7a65c0cad6787cb
Pranav Sonawane <pranav77700@gmail.com>
2024-10-02 22:44:00 +0530

    do while loop

---
+++ 66_dowhile.c
@@ -0,0 +1,20 @@
+/*
+	Description :- Loops(do while loop)
+	Author :- Pranav R Sonawane
+*/
+
+// do while loop
+
+#include<stdio.h>
+int main()
+{
+	int iCnt = 0; // Loop Counter
+	
+	do
+	{
+		printf("Jay Ganesh\n");
+		iCnt++;
+	}while(iCnt < 5);
+	
+	return 0;
+}
